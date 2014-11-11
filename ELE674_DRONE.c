3550a625c816eacef23206dd249dc8f4dc71fcea
Julien Limoges <dev.limoges@gmail.com>
2014-11-10 20:58:48 -0500

    initial commit

---
+++ ELE674_DRONE.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : ELE674_DRONE.c
+ Author      : MS_JL
+ Version     :
+ Copyright   : DON'T COPY
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main(void) {
+	puts("!!!Hello World!!!"); // prints !!!Hello World!!!
+	return EXIT_SUCCESS;
+}*/
+
